f523f60611f69cb88a43cee4d89d5ad58353d4ac
obaeza16 <oscarmaremar@gmail.com>
2021-02-01 14:51:50 +0100

    Lunes 1/02

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ buttons.forEach((element) => {
 		style.href = "css/" + element.value + ".css";
 	});
 });
+
+// Animate TITLES on SCROLL
+var titol1 = document.querySelector("#titol");
+var titol2 = document.querySelector("#exp");
+var titol3 = document.querySelector("#studyTitle");
+// var titol4 = document.querySelector("#titol");
+
+window.addEventListener("scroll", () => {
+	console.log(window.scrollY);
+	if (window.scrollY > 400) {
+		titol1.style.display = "block";
+		titol1.classList.add("animate__animated", "animate__fadeInDown");
+	}
+	if (window.scrollY > 500) {
+		titol2.style.display = "block";
+		titol2.classList.add("animate__animated", "animate__fadeInDown");
+	}
+});
