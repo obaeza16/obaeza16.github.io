df3d951e8f178a4a94a6f4f24a0c150463695c2c
obaeza16 <oscarmaremar@gmail.com>
2021-02-02 11:06:41 +0100

    Add viewport function

--- index.js
+++ index.js
@@ -21,13 +21,43 @@ var titol3 = document.querySelector("#studyTitle");
 // var titol4 = document.querySelector("#titol");
 
 window.addEventListener("scroll", () => {
-	console.log(window.scrollY);
-	if (window.scrollY > 400) {
+	if (window.scrollY > 375) {
 		titol1.style.display = "block";
 		titol1.classList.add("animate__animated", "animate__fadeInDown");
 	}
-	if (window.scrollY > 500) {
+	if (isInViewport(titol2)) {
 		titol2.style.display = "block";
 		titol2.classList.add("animate__animated", "animate__fadeInDown");
 	}
+	if (isInViewport(titol3)) {
+		titol3.style.display = "block";
+		titol3.classList.add("animate__animated", "animate__fadeInDown");
+	}
 });
+
+// To check if element is in viewport
+function isInViewport(element) {
+	const rect = element.getBoundingClientRect();
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <=
+			(window.innerHeight || document.documentElement.clientHeight) &&
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+}
+
+// Testing the viewport function
+// document.addEventListener(
+// 	"scroll",
+// 	function () {
+// 		const messageText = isInViewport(titol1)
+// 			? "The titol 1 is visible in the viewport"
+// 			: "The titol 1 is not visible in the viewport";
+
+// 		console.log(messageText);
+// 	},
+// 	{
+// 		passive: true,
+// 	}
+// );
